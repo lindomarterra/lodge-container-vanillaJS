487b0cf93b4b676665e6f923bb160ccb38c3274b
Terra <terra.lindomar@gmail.com>
2024-01-15 11:18:25 -0300

    jan 15 2024 11:20

---
+++ script/booking.js
@@ -0,0 +1,20 @@
+
+function showCalendar(){
+    document.getElementById('arrowdowncalendar').style.display='none'
+    document.getElementById('arrowupcalendar').style.display='block'
+}
+
+function hideCalendar(){
+    document.getElementById('arrowdowncalendar').style.display='block'
+    document.getElementById('arrowupcalendar').style.display='none'
+}
+
+function showHospedes(){
+    document.getElementById('arrowdownhospedes').style.display='none'
+    document.getElementById('arrowuphospedes').style.display='block'
+}
+
+function hideHospedes(){
+    document.getElementById('arrowdownhospedes').style.display='block'
+    document.getElementById('arrowuphospedes').style.display='none'
+}
